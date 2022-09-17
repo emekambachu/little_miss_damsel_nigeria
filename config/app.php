e6269a94fa2e540c12c7086475f1f66c0f65e849
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-09-17 11:29:35 +0100

    update

--- config/app.php
+++ config/app.php
@@ -227,7 +227,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
         'Image' => Intervention\Image\Facades\Image::class
 
     ],
